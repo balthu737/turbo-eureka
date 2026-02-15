003a0d97fc3b9d2dfab29150028d4349271f5a48
Bigipiex <bg3877102@gmail.com>
2026-02-15 16:23:01 -0500

    chore: guardar cambios

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from service.core.service import Gasto, Usuarios
 query = Querys()
 gastos = Gasto()
 usuarios = Usuarios()
-TOKEN = "7120669603:AAEt85GZrxUWSCt104Bg0ZmS9QbFiURa21k"
+TOKEN = "8017156472:AAFQbaQNjtlhS2KCe9W_4-MxCR-2Wm9CKRc"
 
 def guardar_gasto(monto, categoria):
     query.insetar_movimiento(1, monto, categoria)
