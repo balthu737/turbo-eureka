e90ae85165d9164e36be7766bc1b1a69369635c5
Bigipiex <bg3877102@gmail.com>
2026-02-15 16:16:55 -0500

    'Fix funciones anidadas.'

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from bot.telegram_bot import bot
 from database.query import Querys
-from service.core.mensaje import message_handler
+from service.core.mensaje import analisis
 from service.core.service import Gasto, Usuarios
 
 query = Querys()
 gastos = Gasto()
 usuarios = Usuarios()
-TOKEN = "8017156472:AAFQbaQNjtlhS2KCe9W_4-MxCR-2Wm9CKRc"
+TOKEN = "7120669603:AAEt85GZrxUWSCt104Bg0ZmS9QbFiURa21k"
+
+def guardar_gasto(monto, categoria):
+    query.insetar_movimiento(1, monto, categoria)
+
 def mensaje(text):
-    return message_handler(text)
+    analisis(text, guardar_gasto)
+
 bot(TOKEN, mensaje)
--- service/core/mensaje.py
+++ service/core/mensaje.py
@@ -1,20 +1,18 @@
 def analisis(text, funcion):
-    """
-    Docstring for analisis
-    
-    :param text: Mensaje del usuario
-    :param funcion: Funcion para añadir gastos
-    """
     texto = text.lower().strip().split()
     if not texto:
         print("Formato inválido")
+        return
     if len(texto) < 3:
         print("Formato inválido: gasto <monto> <categoria>")
-    if texto[0] != "gasto" or "ahorro":
+        return
+    if texto[0] != "gasto":
         print("El comando debe empezar con 'gasto'")
+        return
     if not texto[1].isnumeric():
         print("El monto debe ser un número")
+        return
     monto = int(texto[1])
     categoria = texto[2]
-    funcion(monto,categoria)
+    funcion(monto, categoria)
     print(f"Gasto registrado: {monto} en {categoria}")
