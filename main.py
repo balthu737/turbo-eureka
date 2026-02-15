03793218c72b771777d507e71c4747a9cf3233f5
balthu <balthu737@gmail.con>
2026-02-15 18:32:40 -0300

    comiteando

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from bot.telegram_bot import bot
 from database.query import Querys
-from service.core.mensaje import message_handler
+from service.core.mensaje import analisis
 from service.core.service import Gasto, Usuarios
 
-query = Querys()
-gastos = Gasto()
-usuarios = Usuarios()
 TOKEN = "8017156472:AAFQbaQNjtlhS2KCe9W_4-MxCR-2Wm9CKRc"
 def mensaje(text):
-    return message_handler(text)
+    return analisis(text)
 bot(TOKEN, mensaje)
