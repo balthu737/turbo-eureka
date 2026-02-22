057c376c37733ae183311885a9d605d60f21ef42
balthu <balthu737@gmail.con>
2026-02-22 11:28:07 -0300

    agregando la db

--- database/query.py
+++ database/query.py
@@ -2,12 +2,13 @@ from database.conexcion import conexion
 from datetime import datetime
 import pandas as pd
 class   Querys():
-    def insetar_movimiento(self, usuario_id, amount, category_id=None, date=None):
+    def insetar_movimiento(self, usuario_id, amount, category_id, date=None):
         if date is None:
             date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         conn = conexion()
         cursor = conn.cursor()
-        query = f'INSERT INTO movimiento (usuario_id, monto, categoria_id, fecha) VALUES ({usuario_id}, {amount}, {category_id}, "{date}")'
+        id = self.id_categoria(category_id)
+        query = f'INSERT INTO movimiento (usuario_id, monto, categoria_id, fecha) VALUES ({usuario_id}, {amount}, {id}, "{date}")'
         cursor.execute(query)
         cursor.close()
         conn.commit()
@@ -31,6 +32,18 @@ Listando tabla de categorias:
 {df}
 """)
         return df
+    def id_categoria(self, categoria):
+        conn = conexion()
+        cursor = conn.cursor()
+        query = f'SELECT id FROM categoria WHERE nombre = "{categoria}"'
+        query_2 = f'INSERT INTO categoria (nombre) VALUES ("{categoria}")'
+        cursor.execute(query)
+        resultado = cursor.fetchall()
+        if resultado:
+            return resultado[0]
+        cursor.execute(query_2)
+        conn.commit()
+        return cursor.lastrowid
     def hoy(self):
         conn = conexion()
         query = f''
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from bot.telegram_bot import bot
 from database.query import Querys
 from service.core.mensaje import analisis
 from service.core.service import Gasto, Usuarios
-
+from config import TOKEN
 query = Querys()
 gastos = Gasto()
 usuarios = Usuarios()
-TOKEN = "8017156472:AAFQbaQNjtlhS2KCe9W_4-MxCR-2Wm9CKRc"
+token = TOKEN
 
 def guardar_gasto(monto, categoria):
     query.insetar_movimiento(1, monto, categoria)
@@ -14,4 +14,4 @@ def guardar_gasto(monto, categoria):
 def mensaje(text):
     analisis(text, guardar_gasto)
 
-bot(TOKEN, mensaje)
+bot(token, mensaje)
